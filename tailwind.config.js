1313daa870120ecda0fd618bbeb268ffb18691b7
Ademola Ademeso <desmondademeso@gmail.com>
2024-12-21 20:03:11 +0100

    chore(ui): customize text and heading fonts in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        headingsFont: ["Itim", "cursive"],
+        textFont: ["Istok Web", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
