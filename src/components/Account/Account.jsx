9d99c8bb09b3db7f67716f3cd3cb30569a4d5ded
Ademola Ademeso <desmondademeso@gmail.com>
2024-12-24 23:41:56 +0100

    feat(account): integrate logout functionality with Context API and display profile information using mock API

--- src/components/Account/Account.jsx
+++ src/components/Account/Account.jsx
@@ -1,17 +1,18 @@
 import { useNavigate, Link } from "react-router-dom";
 import { useForm } from "react-hook-form";
-// import { useAuth } from "../Auths/Auth";
-import { useEffect } from "react";
+import { useAuth } from "../Auth/AuthContext";
+import { useEffect, useState } from "react";
 
 const Account = () => {
-  // const auth = useAuth();
+  const { logout } = useAuth();
   const navigate = useNavigate();
   const { register, setValue } = useForm();
+  const [userWelcome, setUserWelcome] = useState();
 
   useEffect(() => {
     const token = localStorage.getItem("token");
 
-    fetch("http://localhost:3000/api/profile", {
+    fetch("https://fakestoreapi.com/users/6", {
       method: "GET",
       headers: {
         "Content-Type": "application/json",
@@ -20,10 +21,16 @@ const Account = () => {
     })
       .then((response) => response.json())
       .then((data) => {
-        if (data.status === "success") {
-          setValue("fName", data.profile.firstName || "");
-          setValue("lName", data.profile.lastName || "");
-          setValue("email", data.profile.email || "");
+        if (data) {
+          setValue("fName", data.name.firstname || "");
+          setValue("lName", data.name.lastname || "");
+          setValue("email", data.email || "");
+          setValue(
+            "address",
+            `${data.address.number}, ${data.address.street}, ${data.address.city}` ||
+              ""
+          );
+          setUserWelcome(`${data.name.firstname} ${data.name.lastname}`);
         }
       })
       .catch((error) => {
@@ -32,9 +39,10 @@ const Account = () => {
   }, [setValue]);
 
   const handleLogout = () => {
-    // auth.logout();
-    navigate("/");
+    logout();
+    navigate("/login");
   };
+
   return (
     <>
       <div className="flex my-[50px] mx-16 justify-between">
@@ -42,43 +50,50 @@ const Account = () => {
           <span className="text-[#0000006c]">Home / </span>My Account
         </div>
         <div>
-          Welcome! <span className="text-[#DB4444]">{}</span>
+          Welcome! <span className="text-[#DB4444]">{userWelcome}</span>
         </div>
       </div>
       <div className="grid grid-flow-col ml-16 mr-5">
-        <div className="flex flex-col gap-3">
+        <div className="flex flex-col gap-3 text-[#0000006c]">
           <div>
-            <p className="font-headingsFont">Manage My Account</p>
+            <p className="font-headingsFont text-black">Manage My Account</p>
             <div className="flex flex-col pl-7">
               <Link to="/account" className="hover:text-[#DB4444]">
                 My Profile
               </Link>
-              <Link>Address Book</Link>
+              <Link className="hover:text-[#DB4444]">Address Book</Link>
               <Link to="/checkout" className="hover:text-[#DB4444]">
                 My Payment Options
               </Link>
             </div>
           </div>
           <div>
-            <p className="font-headingsFont">My Orders</p>
+            <p className="font-headingsFont text-black">My Orders</p>
             <div className="flex flex-col pl-7">
-              <Link>My Returns</Link>
-              <Link>My Cancellations</Link>
+              <Link className="hover:text-[#DB4444]">My Returns</Link>
+              <Link className="hover:text-[#DB4444]">My Cancellations</Link>
             </div>
           </div>
           <div>
-            <p className="font-headingsFont">My Wishlist</p>
+            <p className="font-headingsFont text-black">My Wishlist</p>
+            <div className="flex flex-col pl-7">
+              <Link to="/wishlist" className="hover:text-[#DB4444]">
+                My Wishlist
+              </Link>
+            </div>
           </div>
         </div>
         <div className="mt-24 p-10 shadow-md">
-          <p className="font-headingsFont text-[#DB4444]">View Your Profile</p>
+          <p className="font-headingsFont text-[#DB4444] text-lg">
+            View Your Profile
+          </p>
           <form noValidate className="grid grid-cols-2 gap-2">
             <div className="flex flex-col">
               <label htmlFor="fName">First Name</label>
               <input
                 type="text"
                 id="fName"
-                className="p-1"
+                className="p-1 px-2 text-[#0000006c]"
                 disabled
                 {...register("fName")}
               />
@@ -88,7 +103,7 @@ const Account = () => {
               <input
                 type="text"
                 id="lName"
-                className="p-1"
+                className="p-1 px-2 text-[#0000006c]"
                 disabled
                 {...register("lName")}
               />
@@ -98,20 +113,19 @@ const Account = () => {
               <input
                 type="email"
                 id="email"
-                className="p-1"
+                className="p-1 px-2 text-[#0000006c]"
                 disabled
                 {...register("email")}
               />
             </div>
             <div className="flex flex-col">
-              <label htmlFor="password">Password</label>
+              <label htmlFor="address">Address</label>
               <input
-                type="password"
-                id="password"
-                placeholder="********"
-                className="p-1"
+                type="text"
+                id="address"
+                className="p-1 px-2 text-[#0000006c]"
                 disabled
-                {...register("password")}
+                {...register("address")}
               />
             </div>
             <button
